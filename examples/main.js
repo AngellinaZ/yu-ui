93af3168a9d1deef8ba42b09d7768bf062a856a4
zhangyingping <zhangyingping@jiuxiniot.com>
2019-07-26 16:46:34 +0800

    feat: 添加组件相关文件

---
+++ examples/main.js
@@ -0,0 +1,11 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './app'
+import router from './router'
+
+new Vue({
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
+})
